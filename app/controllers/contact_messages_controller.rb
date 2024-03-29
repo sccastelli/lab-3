37c1f7378a0cdfd92b60d4736d072e36cd54c6ac
sccastelli <sccastelli@miuandes.cl>
2024-03-29 10:30:47 -0300

    arreglos

--- app/controllers/contact_messages_controller.rb
+++ app/controllers/contact_messages_controller.rb
@@ -1,2 +1,21 @@
 class ContactMessagesController < ApplicationController
-end
+    def new
+      @contact_message = ContactMessage.new
+    end
+  
+    def create
+      @contact_message = ContactMessage.new(contact_message_params)
+      if @contact_message.save
+        redirect_to contact_path, notice: "Thank you for your message, we will contact you soon."
+      else
+        render :new
+      end
+    end
+  
+    private
+  
+    def contact_message_params
+      params.require(:contact_message).permit(:name, :email, :message)
+    end
+  end
+  
