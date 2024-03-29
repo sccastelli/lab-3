f2f9ef884e7d2e230ac25969e08a80e845a8ea5c
sccastelli <sccastelli@miuandes.cl>
2024-03-28 23:16:49 -0300

    Primer commit en la rama main

---
+++ config/routes.rb
@@ -0,0 +1,20 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  #para inicio
+  root 'pages#home'
+
+  #para las paginas
+  get 'about', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
+  
+  #para el formulario de contacto
+  post 'contact', to: 'contact_messages#create'
+
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get "up" => "rails/health#show", as: :rails_health_check
+
+  # Defines the root path route ("/")
+  # root "posts#index"
+end
